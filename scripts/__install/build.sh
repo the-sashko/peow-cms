dced82723db678a5c2b7bc3311b4b31f0d437f0b
The Sashko <iskander.bondarenko@gmail.com>
2017-01-17 15:16:04 +0200

    build script fix

--- scripts/__install/build.sh
+++ scripts/__install/build.sh
@@ -2,7 +2,7 @@
 project_path="$(pwd)/$(dirname $0)"
 cd "${project_path/'/./'/'/'}"
 curr_time=$(date '+%Y-%m-%d_%H-%M-%S')
-zip -r tmp/$curr_time.zip build > /dev/null
+zip -r tmp/$curr_time.zip build> /dev/null
 cp tmp/$curr_time.zip back/$curr_time.zip
 x=$(ls back | wc -l)
 if [ $(ls back | wc -l) -gt 5 ]; then
@@ -10,9 +10,8 @@ if [ $(ls back | wc -l) -gt 5 ]; then
 		rm "back/$(ls -t back | tail -1)"
 	done
 fi
-rm -rf build/* > /dev/null
-rm -rf build/.* > /dev/null
+rm -rf build/*
 ./main.py
-rm -rf tmp/* > /dev/null
-rm -rf tmp/.* > /dev/null
+cp -r tmp/build/* build
+rm -rf tmp/*
 ./scripts/send.sh
