56524c30be2b4e977ffa79d52858b63b642872de
The Sashko <iskander.bondarenko@gmail.com>
2017-01-17 16:09:51 +0200

    fixed install script for Ubuntu

--- scripts/__install/ubuntu.sh
+++ scripts/__install/ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get install zip virtualenv virtualenvwrapper python3 python3-pip python3-virtualenv
-sudo pip install --upgrade pip
-sudo pip3 install --upgrade pip
-sudo pip3 install markdown2
-sudo pip3 install transliterate
-sudo pip3 install pillow
+sudo -H pip install --upgrade pip
+sudo -H pip3 install --upgrade pip
+sudo -H pip3 install markdown2
+sudo -H pip3 install transliterate
+sudo -H pip3 install pillow
