c585f98ce84aa8d62d710ca3d15b194f9604daec
The Sashko <iskander.bondarenko@gmail.com>
2017-01-18 14:46:05 +0200

    some bugs fixed

--- tools/auth.py
+++ tools/auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import json,os
-print('Sending...')
+print('Adding ssh-key to remote server...')
 serverParams=json.loads(open('config/ssh.json','r').read())
 os.system('cat ~/.ssh/id_rsa.pub | ssh '+serverParams['user']+'@'+serverParams['host']+' "mkdir -p ~/.ssh && cat >>  ~/.ssh/authorized_keys"')
--- tools/git.py
+++ tools/git.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import json,os
 print('Commit to git ...')
-gitParams=json.loads(open('config/ssh.json','r').read())
+gitParams=json.loads(open('config/git.json','r').read())
 os.system('git clone '+gitParams['repo']+' tmp/git')
