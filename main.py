8928345e01b733155642141ad57dd309b7fe5da5
The Sashko <iskander.bondarenko@gmail.com>
2017-01-18 18:20:17 +0200

    add global try-except

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 from app import main
 import sys
-#try:
-main.init()
-#except Exception as e:
-#	print('ERROR!')
-#	print(e)
-#	sys.exit()
+try:
+  main.init()
+except Exception as e:
+  print('ERROR!')
+  print(e)
+sys.exit()
