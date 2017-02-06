23d6251a5f8d96f94abc7ad1939afc1cf8ab7e17
The Sashko <iskander.bondarenko@gmail.com>
2017-02-06 14:08:33 +0200

    css version bug fix

--- app/minify/__init__.py
+++ app/minify/__init__.py
@@ -2,7 +2,7 @@ import requests
 
 def css(version):
 	cssMin=requests.post("http://cssminifier.com/raw", data={"input":open("content/assets/css/style.css", "r").read()}).text
-	open("tmp/build/assets/css/style.min.css?v="+version, "w+").write(cssMin)
+	open("tmp/build/assets/css/style.min.css", "w+").write(cssMin)
 	return 1
 
 def js():
